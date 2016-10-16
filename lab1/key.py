4fb2d53cccd4618c6fc86714ed20e77812640512
Mehmet Tugrul Savran <tugrul.savran@gmail.com>
2016-10-15 23:18:44 -0400

    Revert "Assignment Solutions for MIT Artificial Intelligence Fall 2016"
    This reverts commit d95bb4db96f8df074dd0789c85a6327523276f02.

--- lab1/key.py
+++
@@ -1,6 +0,0 @@
-# Login information for the 6.034 Automated Tester
-
-USERNAME="tugrul_MIT_EDU"
-PASSWORD="Y3XQtMBkpxu54RvasaDh"
-XMLRPC_URL="https://ai6034.mit.edu/labs/xmlrpc/"
-
--- lab2/key.py
+++
@@ -1,6 +0,0 @@
-# Login information for the 6.034 Automated Tester
-
-USERNAME="tugrul_MIT_EDU"
-PASSWORD="hdse3RxWDAf9JEQGj4sD"
-XMLRPC_URL="https://ai6034.mit.edu/labs/xmlrpc/"
-
